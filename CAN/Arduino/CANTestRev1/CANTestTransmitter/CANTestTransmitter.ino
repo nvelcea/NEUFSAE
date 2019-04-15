12c0396fd8227cc203d20417dd1e050dfb9af7b5
nvelcea <nvelcea@gmail.com>
2019-04-14 22:45:22 -0400

    Changed folder name, commented & cleaned Transmitter

--- CAN/Arduino/CANTestRev1/CANTestTransmitter/CANTestTransmitter.ino
+++ CAN/Arduino/CANTestRev1/CANTestTransmitter/CANTestTransmitter.ino
@@ -4,19 +4,20 @@
 
 //Cant Initialize Shield IDK
 
+//REMEMBER TO SWITCH PINS 11 AND 12
 
-
-int potPin = A0; 
-int potVal = 0; 
-const int spiCSPin = 10;
+const int canId = 0x43; 
+const int potPin = A0; //input for potentiometer
+int potVal = 0; //analog input value
+const int spiCSPin = 10; //base CAN pin
 
 MCP_CAN CAN(spiCSPin);
 
 void setup()
 {
-    Serial.begin(115200);
+    Serial.begin(115200); //baud rate for CAN
 
-    while (CAN_OK != CAN.begin(CAN_500KBPS, MCP_8MHz))
+    while (CAN_OK != CAN.begin(CAN_500KBPS, MCP_8MHz)) //specify 8MHz crystal
     { 
         Serial.println("CAN BUS init Failed");
         delay(250);
@@ -28,9 +29,10 @@ unsigned char stmp[8] = {0, 0, 0, 0, 0, 0, 0, 0};
     
 void loop()
 {   
+  //read analog input, add to CAN message
    potVal = analogRead(potPin); 
    potVal = potVal /4; //makes it max of 256 --> one byte. 
    stmp[0] = potVal; 
-  CAN.sendMsgBuf(0x43, 0, 8, stmp);
-  delay(1000);
+  CAN.sendMsgBuf(canId, 0, 8, stmp);
+  delay(100);
 }
