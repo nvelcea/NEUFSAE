8c682f460ecf39bb1ef0771bdc202173181a6f9a
nvelcea <nvelcea@gmail.com>
2019-04-15 21:12:43 -0400

    got 0x01 and 0x02 working

--- CAN/Arduino/CANTestRev2/0x01/0x01.ino
+++ CAN/Arduino/CANTestRev2/0x01/0x01.ino
@@ -12,19 +12,28 @@
 #include <mcp_can.h>
 #include <mcp_can_dfs.h>
 #include <SPI.h>
+#include <LiquidCrystal.h>
 
-
-const in canId = 0x01; 
+const int canId = 0x01; 
 const int spiCSPin = 10; //base CAN pin
 unsigned char stmp[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 unsigned char len = 0;
 unsigned char buf[8];
 
+int potVal = 0; 
+int potPin = A0; 
+
+float accel[3] = {0, 0, 0};
+
+void decodeAccelValues(float out[3], unsigned char in[8]); 
+
 MCP_CAN CAN(spiCSPin);
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2); 
 void setup()
 {
     Serial.begin(115200); //baud rate for CAN
+    lcd.begin(16,2);
 
     while (CAN_OK != CAN.begin(CAN_500KBPS, MCP_8MHz)) //specify 8MHz crystal
     { 
@@ -49,9 +58,49 @@ void loop() {
     unsigned long canId = CAN.getCanId(); //gets canID
 
     //if from 0x02
-
+    if(canId = 0x02){
+      decodeAccelValues(accel, buf); 
+    }
     //if from 0x03
 
        } 
   //update LCD
+  lcd.setCursor(0,0); 
+  lcd.print("X:"); 
+  if(accel[0]>=0) lcd.print("+"); 
+  lcd.print(accel[0],2); 
+ 
+  lcd.setCursor(8,0);
+  lcd.print("Y:"); 
+  if(accel[1]>=0) lcd.print("+"); 
+  lcd.print(accel[1],2); 
+
+  lcd.setCursor(0,1);
+  lcd.print("Z:"); 
+  if(accel[2]>=0) lcd.print("+"); 
+  lcd.print(accel[2],2); 
+  
+  delay(100); //NOTE: a larger delay reduces the updates per second, and can therefore make the display
+  //more readable
 }
+void decodeAccelValues(float out[3], unsigned char in[8]){
+  if(in[0] == 0){
+    out[0] = -1*(float)in[1]/64;
+  }
+  if(in[0] == 1){
+    out[0] = 1*(float)in[1]/64;
+  }
+  if(in[2] == 0){
+    out[1] = -1*(float)in[3]/64;
+  }
+  if(in[2] == 1){
+    out[1] = 1*(float)in[3]/64;
+  }
+  if(in[4] == 0){
+    out[2] = -1*(float)in[5]/64;
+  }
+  if(in[4] == 1){
+    out[2] = 1*(float)in[5]/64;
+  }
+}
+
--- CAN/Arduino/CANTestRev2/0x02/0x02.ino
+++ CAN/Arduino/CANTestRev2/0x02/0x02.ino
@@ -1,26 +1,38 @@
+#include <SFE_MMA8452Q.h>
+
 //By Nicholas Velcea
 //4-15-19
 //0x02:
 //  Accelerometer input
 //  OutputMessage:
-//  0 - X axis Value
-//  1 - Y axis Value
-//  2 - Z axis Value
-//    Note: Values will be shifted to 0 - 255, 127 = 0, 0 is max negative, 255 is max positive
+//  0 - X axis Sign
+//  1 - X axis Value
+//  2 - Y axis Sign
+//  3 - Y axis Value
+//  4 - Z axis Sign
+//  5 - Z axis Value
+//    Note: 
 
 #include <mcp_can.h>
 #include <mcp_can_dfs.h>
 #include <SPI.h>
+#include <SFE_MMA8452Q.h>
 
-
-const in canId = 0x02; 
+const int canId = 0x02; 
 const int spiCSPin = 10; //base CAN pin
 unsigned char stmp[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
+float accels[3]; 
+
+MMA8452Q accel; 
 MCP_CAN CAN(spiCSPin);
+
 void setup()
 {
     Serial.begin(115200); //baud rate for CAN
+    
+    accel.init(SCALE_4G); 
+    Serial.println("HOWDY");
 
     while (CAN_OK != CAN.begin(CAN_500KBPS, MCP_8MHz)) //specify 8MHz crystal
     { 
@@ -34,5 +46,39 @@ void setup()
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  if(accel.available()){
+    accel.read(); 
 
+    accels[0] = accel.cx; 
+    accels[1] = accel.cy; 
+    accels[2] = accel.cz; 
+
+    if(accels[0]<0){
+      stmp[0] = 0; 
+      stmp[1] = -1.0*accels[0]*64; 
+    }
+    if(accels[0]>0){
+      stmp[0] = 1; 
+      stmp[1] = 1.0*accels[0]*64; 
+    }
+    if(accels[1]<0){
+      stmp[2] = 0; 
+      stmp[3] = -1.0*accels[1]*64; 
+    }
+    if(accels[1]>0){
+      stmp[2] = 1; 
+      stmp[3] = 1.0*accels[1]*64; 
+    }
+    if(accels[2]<0){
+      stmp[4] = 0; 
+      stmp[5] = -1.0*accels[2]*64; 
+    }
+    if(accels[1]>0){
+      stmp[4] = 1; 
+      stmp[5] = 1.0*accels[2]*64; 
+    }
+    CAN.sendMsgBuf(canId, 0, 8, stmp); 
+  }
+  delay(10); 
 }
