5992fe6c8c58edacf4f94a714f224e4ea97b0dcb
nvelcea <nvelcea@gmail.com>
2019-04-16 00:25:03 -0400

    Update 0x02.ino

--- CAN/Arduino/CANTestRev2/0x02/0x02.ino
+++ CAN/Arduino/CANTestRev2/0x02/0x02.ino
@@ -11,74 +11,83 @@
 //  3 - Y axis Value
 //  4 - Z axis Sign
 //  5 - Z axis Value
-//    Note: 
+//    Note:
 
 #include <mcp_can.h>
 #include <mcp_can_dfs.h>
 #include <SPI.h>
 #include <SFE_MMA8452Q.h>
 
-const int canId = 0x02; 
+const int canId = 0x02;
 const int spiCSPin = 10; //base CAN pin
 unsigned char stmp[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-float accels[3]; 
+float accels[3];
 
-MMA8452Q accel; 
+MMA8452Q accel;
 MCP_CAN CAN(spiCSPin);
+
+void updateAccels(float accels[3], unsigned char stmp[8]);
 
 void setup()
 {
-    Serial.begin(115200); //baud rate for CAN
-    
-    accel.init(SCALE_4G); 
-    Serial.println("HOWDY");
+  Serial.begin(115200); //baud rate for CAN
 
-    while (CAN_OK != CAN.begin(CAN_500KBPS, MCP_8MHz)) //specify 8MHz crystal
-    { 
-        Serial.println("CAN BUS init Failed");
-        delay(250);
-    }
-    Serial.println("CAN BUS Shield Init OK!");
+  accel.init(SCALE_4G); //scaled to +-4G because we realistically wont go higher than that, and will allow for more precise conversion
+  Serial.println("HOWDY");
+
+  while (CAN_OK != CAN.begin(CAN_500KBPS, MCP_8MHz)) //specify 8MHz crystal
+  {
+    Serial.println("CAN BUS init Failed");
+    delay(250);
+  }
+  Serial.println("CAN BUS Shield Init OK!");
 }
 
 
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  if(accel.available()){
-    accel.read(); 
 
-    accels[0] = accel.cx; 
-    accels[1] = accel.cy; 
-    accels[2] = accel.cz; 
+  if (accel.available()) {
+    accel.read();
 
-    if(accels[0]<0){
-      stmp[0] = 0; 
-      stmp[1] = -1.0*accels[0]*64; 
-    }
-    if(accels[0]>0){
-      stmp[0] = 1; 
-      stmp[1] = 1.0*accels[0]*64; 
-    }
-    if(accels[1]<0){
-      stmp[2] = 0; 
-      stmp[3] = -1.0*accels[1]*64; 
-    }
-    if(accels[1]>0){
-      stmp[2] = 1; 
-      stmp[3] = 1.0*accels[1]*64; 
-    }
-    if(accels[2]<0){
-      stmp[4] = 0; 
-      stmp[5] = -1.0*accels[2]*64; 
-    }
-    if(accels[1]>0){
-      stmp[4] = 1; 
-      stmp[5] = 1.0*accels[2]*64; 
-    }
-    CAN.sendMsgBuf(canId, 0, 8, stmp); 
+    accels[0] = accel.cx;
+    accels[1] = accel.cy;
+    accels[2] = accel.cz;
+
+    updateAccels(accels, stmp);
+    CAN.sendMsgBuf(canId, 0, 8, stmp);
   }
-  delay(10); 
+  delay(10);
 }
+
+void updateAccels(float accels[3], unsigned char stmp[8]) {
+  /* This function converts accel data from the accelerometer into a message that can be trasmitted over can
+  */
+  if (accels[0] < 0) {
+    stmp[0] = 0;
+    stmp[1] = -1.0 * accels[0] * 64;
+  }
+  if (accels[0] > 0) {
+    stmp[0] = 1;
+    stmp[1] = 1.0 * accels[0] * 64;
+  }
+  if (accels[1] < 0) {
+    stmp[2] = 0;
+    stmp[3] = -1.0 * accels[1] * 64;
+  }
+  if (accels[1] > 0) {
+    stmp[2] = 1;
+    stmp[3] = 1.0 * accels[1] * 64;
+  }
+  if (accels[2] < 0) {
+    stmp[4] = 0;
+    stmp[5] = -1.0 * accels[2] * 64;
+  }
+  if (accels[1] > 0) {
+    stmp[4] = 1;
+    stmp[5] = 1.0 * accels[2] * 64;
+  }
+}
+
